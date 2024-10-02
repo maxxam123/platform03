47ba809a69bc7e9c013ef20e05b6e403f2d18146
maxxam123 <hennighausenmax@gmail.coom>
2024-10-02 01:34:24 -0700

    added variables to variables.tf

--- cluster/aws01/variables.tf
+++ cluster/aws01/variables.tf
@@ -3,3 +3,15 @@ variable "instance_type" {
   type = string
   default = "t2.micro"
 }
+
+variable "ami" {
+  description = "ami"
+  type = string
+  default = "ami-01002120012"
+}
+
+variable "subnet_id" {
+  description = "subnet_id"
+  type = string
+  default = "subnet-00e39a3f15e5c70f7"
+}
