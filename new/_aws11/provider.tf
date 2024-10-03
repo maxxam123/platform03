5e43967f0efc7630c30423b07146fdeb86ac6a5e
max <81824845+maxxam123@users.noreply.github.com>
2024-10-03 04:25:13 -0700

    Update provider.tf

--- new/_aws11/provider.tf
+++ new/_aws11/provider.tf
@@ -1,11 +1,11 @@
-aws03
+aws01
 aws
 nginx REPLICAS=1 LOADBALANCER=Loadbalancer
-eso
+eso SECRET=supersecret
 velero URL=https://www.test.com
 certmanager cert=cert01
 prometheus REPLICAS=1
-thanos REPLICAS=1
+thanos
 
 
 
