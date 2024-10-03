225dfe976557968518ee8ffc49e7fb36cc9cd441
maxxam123 <hennighausenmax@gmail.com>
2024-10-03 14:49:58 +0000

    Update from https://github.com/maxxam123/platform03/commit/4cdfb9747d6b2161b31b4e88655e73f3806c8025

---
+++ cluster/aws03/provider.tf
@@ -0,0 +1,36 @@
+aws03
+aws
+nginx REPLICAS=1 LOADBALANCER=Loadbalancer
+eso SECRET=supersecret
+velero URL=https://www.test.com
+certmanager cert=cert01
+prometheus REPLICAS=1
+thanos REPLICAS=1
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
